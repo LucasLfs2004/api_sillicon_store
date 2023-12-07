d6ece44de696db55cb2ff371beab7c019d8a5f7a
LucasLfs2004 <lucas.lfs2004@gmail.com>
2023-12-07 14:55:53 -0300

    Rota de categoria concluída

--- api/routers/category.py
+++ api/routers/category.py
@@ -10,151 +10,127 @@ router = APIRouter()
 
 upload_folder = "public/category"
 
+
 @router.get("/category", tags=["Categoria"])
 def get_categorys():
-    try: 
+    try:
         cursor = mysql_connection.cursor(dictionary=True)
         cursor.execute("SELECT * FROM category")
         categorys = cursor.fetchall()
 
         cursor.close()
-        return {"categorys:": categorys}
+        return categorys
     except Exception as e:
         cursor.close()
         raise HTTPException(status_code=500, detail=str(e))
 
 
-
 @router.post("/category", tags=['Categoria'])
-def create_category(name: str = Form(), brand_logo: UploadFile = File(...), brand_logo_black: UploadFile = File(...)):
+def create_category(name: str = Form(), path_img: UploadFile = File(None)):
     try:
         cursor = mysql_connection.cursor(dictionary=True)
-        id = int.from_bytes(uuid.uuid4().bytes[:4], byteorder="big") % (2 ** 32)
+        id = int.from_bytes(
+            uuid.uuid4().bytes[:4], byteorder="big") % (2 ** 30)
 
-        filename_brand_logo = str(time.time()) + brand_logo.filename.replace(' ', '_')
-        file_path = os.path.join(upload_folder, filename_brand_logo)
-        with open(file_path, "wb") as f:
-            f.write(brand_logo.file.read())
-        print(filename_brand_logo)
-        filename_brand_logo_black = str(time.time()) + brand_logo_black.filename.replace(' ', '_')
-        file_path = os.path.join(upload_folder, filename_brand_logo_black)
-        with open(file_path, "wb") as f:
-            f.write(brand_logo_black.file.read())
-        print(filename_brand_logo_black)
-
-
-        # cursor.execute(
-        #     "INSERT INTO CATEGORY (id, name, brand_logo, brand_logo_black) VALUES (%s, %s %s, %s)",
-        #     (id, name, filename_brand_logo)
-        # )
-        # mysql_connection.commit()
+        if path_img is not None:
+            filename_category = str(time.time()) + \
+                path_img.filename.replace(' ', '_')
+            file_path = os.path.join(upload_folder, filename_category)
+            with open(file_path, "wb") as f:
+                f.write(path_img.file.read())
+            print(filename_category)
+            cursor.execute("INSERT INTO CATEGORY (id, name, path_img) VALUES (%s, %s, %s)",
+                           (id, name, str("public/category/" + filename_category,)))
+        else:
+            cursor.execute("INSERT INTO CATEGORY (id, name) VALUES (%s, %s)",
+                           (id, name,))
+        mysql_connection.commit()
 
         # Realizar consulta para obter os dados inseridos
-        # cursor.execute("SELECT * FROM category WHERE ID = %s", (category.id,))
-        # inserted_data = cursor.fetchone()
+        cursor.execute("SELECT * FROM category WHERE NAME = %s", (name,))
+        inserted_data = cursor.fetchone()
 
         # # Fechar o cursor e retornar o ID do produto
-        # cursor.close()
-        # return {"new_category:": inserted_data}
-        return {
-            "name:": name,
-            "brand_logo_path:": filename_brand_logo, 
-            "brand_logo_black_path:": filename_brand_logo_black, 
-        }
+        cursor.close()
+        return inserted_data
+
     except Exception as e:
         # Em caso de erro, cancelar a transação e retornar uma resposta de erro
         mysql_connection.rollback()
         raise HTTPException(status_code=500, detail=str(e))
 
 
-# @router.post("/product", tags=['Produtos'])
-# async def create_product(owner: str = Form(), name: str = Form(), description: str = Form(), brand: str = Form(), category: str = Form(),
-#                          price: str = Form(), stock: str = Form(), featured: str = Form(), files: List[UploadFile] = File(...)):
-#     filenames = []
-#     time_stamp = calendar.timegm(current_GMT)
-#     product = {
-#         'id': str(uuid.uuid4()),
-#         'owner': owner,
-#         'active': bool(featured),
-#         'featured': True,
-#         'name': name,
-#         'description': description,
-#         'brand': brand,
-#         'category': category,
-#         'price': float(price),
-#         'stock': int(stock),
-#         'color': str('null'),
-#         'created_at': int(time_stamp),
-#         'updated_at': int(time_stamp),
-#     }
-#     try:
-#         # Conectar ao banco de dados
-#         cursor = mysql_connection.cursor(dictionary=True)
+@router.patch("/category", tags=["Categoria"])
+def update_category(old_name: str = Form(), name: str = Form(None), path_img: UploadFile = File(None)):
+    try:
+        cursor = mysql_connection.cursor(dictionary=True)
 
-#         # Executar a inserção na tabela produtos
-#         cursor.execute(
-#             "INSERT INTO PRODUCT (id, owner, name, description, brand, price, stock, active, created_at, updated_at, category, featured) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
-#             (product['id'], product['owner'], product['name'], product['description'], product['brand'], product['price'], product['stock'],
-#              product['active'], product['created_at'], product['updated_at'], product['category'], product['featured'])
-#         )
-#         # Fechar o cursor e retornar o ID do produto
-#         for file in files:
-#             filename = str(time.time()) + file.filename.replace(' ', '_')
-#             filenames.append(filename)
-#             file_path = os.path.join(upload_folder, filename)
-#             with open(file_path, "wb") as f:
-#                 f.write(file.file.read())
-#             cursor.execute(
-#                 "INSERT INTO image (id, path) VALUES (%s, %s)",
-#                 (product['id'], str("public/" + filename))
-#             )
-#         print(filenames)
+        cursor.execute("SELECT * FROM CATEGORY WHERE NAME = %s", (old_name,))
+        old_category = cursor.fetchone()
 
-#         cursor.execute(
-#             "INSERT INTO rating (id, amount, rating) VALUES (%s, %s, %s)",
-#             (product['id'], int(0), int(0))
-#         )
-#         mysql_connection.commit()
+        new_category = {}
 
-#         # Realizar consulta para obter os dados inseridos
-#         cursor.execute("""
-# 									SELECT
-# 											product.id AS id,
-# 											product.name AS product_name,
-# 											product.description AS product_description,
-# 											product.brand AS product_brand,
-# 											product.price AS product_price,
-# 											product.stock AS product_stock,
-# 											product.category AS product_category,
-# 											JSON_ARRAYAGG(
-# 													JSON_OBJECT(
-# 															'img_path', image.path
-# 													)
-# 											) AS images,
-# 											JSON_ARRAYAGG(
-# 													JSON_OBJECT(
-# 															'amount', rating.amount,
-# 															'rating', rating.rating
-# 													)
-# 											) AS rating
-# 									FROM
-# 											product
-# 									LEFT JOIN
-# 											image ON product.id = image.id
-# 									LEFT JOIN
-# 											rating ON product.id = rating.id
-# 									WHERE
-# 											product.id = %s
-# 									GROUP BY
-# 											product.id;
-# 											""", (product['id'],)
-#         )
-#         product_data = cursor.fetchone()
-#         cursor.close()
-#         return {"new_product:": product_data}
-#     except Exception as e:
-#         # Em caso de erro, cancelar a transação e retornar uma resposta de erro
-#         mysql_connection.rollback()
-#         raise HTTPException(status_code=500, detail=str(e))
-#     finally:
-#         cursor.close()
+        if name is not None:
+            new_category["name"] = name
+        else:
+            new_category["name"] = old_name
+
+        if path_img is not None:
+            filename_path_img = str(time.time()) + \
+                path_img.filename.replace(' ', '_')
+            file_path = os.path.join(upload_folder, filename_path_img)
+            with open(file_path, "wb") as f:
+                f.write(path_img.file.read())
+            print(filename_path_img)
+            new_category["path_img"] = str(
+                'public/brand/' + filename_path_img)
+        else:
+            new_category["path_img"] = old_category["path_img"]
+
+        cursor.execute(
+            "UPDATE CATEGORY SET name = %s, path_img = %s WHERE name = %s",
+            (new_category["name"], new_category["path_img"], old_name)
+        )
+        mysql_connection.commit()
+
+        print("Tudo certo até aqui, iniciando a exclusão das imagens antigas")
+
+        # Removendo imagens antigas da api
+        if path_img is not None:
+            filename_logo = os.path.join(old_category["path_img"])
+            os.remove(filename_logo)
+
+        cursor.execute("SELECT * FROM CATEGORY WHERE NAME = %s", (name,))
+        category_updated = cursor.fetchone()
+        cursor.close()
+        return category_updated
+
+    except Exception as e:
+        print(e)
+        return e
+
+
+@router.delete("/category", tags=["Categoria"])
+def delete_category(name: str = Form()):
+    try:
+        cursor = mysql_connection.cursor(dictionary=True)
+        print("Executando select")
+        cursor.execute("SELECT * FROM CATEGORY WHERE NAME = %s",
+                       (name,))
+        category_delete = cursor.fetchone()
+        print("executando DELETE")
+        cursor.execute("DELETE FROM CATEGORY WHERE NAME LIKE %s", (name,)),
+        mysql_connection.commit()
+
+        print(category_delete)
+        if category_delete["path_img"] is not None:
+            filename_logo = os.path.join(category_delete["brand_logo"])
+            os.remove(filename_logo)
+
+        cursor.close()
+        return True
+
+    except Exception as e:
+        cursor.close()
+        print(e)
+        return e
