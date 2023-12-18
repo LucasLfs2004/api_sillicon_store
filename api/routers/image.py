a0d9a5d492d3f0dbf9141fb694482f3543ce9d3c
Lucas Ferreira Silva <lucas.ferreira@pmovil.com.br>
2023-12-18 14:01:19 -0300

    delete unused files

--- api/routers/image.py
+++ api/routers/image.py
@@ -15,3 +15,13 @@ def get_image(image_path: str):
         return FileResponse(file_path)
     return {"error": "File not found"}
     # return image_path
+
+
+@router.get("/image/brand/{image_path}", tags=["Imagens"])
+def get_image(image_path: str):
+    print(image_path)
+    file_path = str("public/brand/" + image_path)
+    if os.path.exists(file_path):
+        return FileResponse(file_path)
+    return {"error": "File not found"}
+    # return image_path
