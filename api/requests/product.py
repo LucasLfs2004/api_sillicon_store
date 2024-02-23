aa03711319955755f94c6d15b6c354774aed3a2a
Lucas Ferreira Silva <lucas.ferreira@pmovil.com.br>
2024-02-23 15:10:04 -0300

    continuação do get de imagens ordenadas

--- api/requests/product.py
+++ api/requests/product.py
@@ -200,116 +200,236 @@ get_limit_products = """
             LIMIT %s;
                 """
 
+new_get_products_limit = """
+            SELECT JSON_ARRAYAGG(
+                    JSON_OBJECT(
+                        'id', product.id, 'name', product.name, 'stock', product.stock, 'warranty', product.warranty, 'model', product.model, 'featured', product.featured, 'active', product.active, 'category', category.name, 'brand', JSON_OBJECT(
+                            'name', brand.name, 'logo', brand.brand_logo, 'logo_black', brand.brand_logo_black
+                        ), 'images', (
+                            Select JSON_ARRAYAGG(JSON_OBJECT('path', image.path, 'index', image.index_image))
+                            from image
+                            WHERE
+                                image.id_product = product.id
+                        ), 'value', JSON_OBJECT(
+                            'price_now', value_product.price_now, 'common_price', value_product.common_price, 'portions', value_product.portions, 'fees_monthly', value_product.fees_monthly, 'fees_credit', value_product.fees_credit
+                        ), 'rating', JSON_OBJECT(
+                            'amount_rating', rating.amount, 'rating_value', rating.rating, 'comments', (select JSON_ARRAYAGG(JSON_OBJECT('id_comment', comment.id_comment, 'title_text', comment.title_text, 'rating_value', comment.rating_value, 'text_comment', comment.comment_text, 'created_at', comment.created_at)) FROM comment where comment.id_product = product.id)
+                        ), 'description', JSON_OBJECT(
+                            'desc_html', description_product.description_html, 'updated_at', description_product.updated_at
+                        )
+                    )
+                ) as products
+            FROM
+                product
+                LEFT JOIN category on product.category_id = category.id
+                LEFT JOIN brand on product.brand_id = brand.id
+                LEFT JOIN value_product on product.id = value_product.id_product
+                LEFT JOIN rating on product.id = rating.id_product
+                LEFT JOIN description_product on product.id = description_product.id_product
+            ORDER BY product.created_at
+            LIMIT %s"""
+
+
 get_product_id = """
-            SELECT
-                JSON_OBJECT(
-                    'id',
-                    product.id,
-                    'name',
-                    product.name,
-                    'stock',
-                    product.stock,
-                    'warranty',
-                    product.warranty,
-                    'model',
-                    product.model,
-                    'featured',
-                    product.featured,
-                    'active',
-                    product.active,
-                    'brand', JSON_OBJECT(
-                        'name', 
-                        brand.name,
-                        'logo',
-                        brand.brand_logo,
-                        'logo_black', brand.brand_logo_black
-                    ),
-                    'category',
-                    category.name,
-                    'value',
-                    JSON_OBJECT(
-                        'price_now',
-                        value_product.price_now,
-                        'common_price',
-                        value_product.common_price,
-                        'portions',
-                        value_product.portions,
-                        'fees_monthly',
-                        value_product.fees_monthly,
-                        'fees_credit',
-                        value_product.fees_credit
-                    ),
-                    'rating',
-                    JSON_OBJECT(
-                        'amount_rating',
-                        rating.amount,
-                        'rating_value',
-                        rating.rating,
-                        'comments',
-                        CASE
-                            WHEN COUNT(comment.id_comment) > 0 THEN JSON_ARRAY(
-                                JSON_OBJECT(
-                                    'id_comment',
-                                    comment.id_comment,
-                                    'rating_value',
-                                    comment.rating_value,
-                                    'title_text',
-                                    comment.title_text,
-                                    'text_comment',
-                                    comment.comment_text
-                                )
-                            )
-                            ELSE JSON_ARRAY()
-                        END
-                    ),
-                    'description',
-                    JSON_OBJECT(
-                        'desc_html',
-                        description_product.description_html,
-                        'updated_at',
-                        description_product.updated_at
-                    ),
-                    'images',
-                    JSON_ARRAYAGG(image.path),
-                    'store_name', seller.store_name
-                ) AS product
-            FROM product
-                LEFT JOIN category ON product.category_id = category.id
-                LEFT JOIN brand ON product.brand_id = brand.id
-                LEFT JOIN value_product ON product.id = value_product.id_product
-                LEFT JOIN rating ON product.id = rating.id_product
-                LEFT JOIN image ON product.id = image.id_product
-                LEFT JOIN comment ON product.id = comment.id_product
-                LEFT JOIN description_product ON product.id = description_product.id_product
-                LEFT JOIN seller ON product.seller_id = seller.id
-                WHERE product.id = %s
-            GROUP BY
-                product.id,
-                product.name,
-                product.stock,
-                product.warranty,
-                product.model,
-                product.featured,
-                product.active,
-                brand.name,
-                brand.brand_logo,
-                brand.brand_logo_black,
-                category.name,
-                value_product.price_now,
-                value_product.common_price,
-                value_product.portions,
-                value_product.fees_monthly,
-                value_product.fees_credit,
-                rating.amount,
-                rating.rating,
-                comment.id_comment,
-                comment.rating_value,
-                comment.title_text,
-                comment.comment_text,
-                description_product.updated_at,
-                description_product.description_html,
-                seller.store_name;
+SELECT
+    JSON_OBJECT (
+        'id',
+        product.id,
+        'name',
+        product.name,
+        'stock',
+        product.stock,
+        'warranty',
+        product.warranty,
+        'model',
+        product.model,
+        'featured',
+        product.featured,
+        'active',
+        product.active,
+        'category',
+        category.name,
+        'brand',
+        JSON_OBJECT (
+            'name',
+            brand.name,
+            'logo',
+            brand.brand_logo,
+            'logo_black',
+            brand.brand_logo_black
+        ),
+        'images',
+        (
+            Select
+                JSON_ARRAYAGG (
+                    JSON_OBJECT ('path', image.path, 'index', image.index_image)
+                )
+            from
+                image
+            WHERE
+                image.id_product = product.id
+        ),
+        'value',
+        JSON_OBJECT (
+            'price_now',
+            value_product.price_now,
+            'common_price',
+            value_product.common_price,
+            'portions',
+            value_product.portions,
+            'fees_monthly',
+            value_product.fees_monthly,
+            'fees_credit',
+            value_product.fees_credit
+        ),
+        'rating',
+        JSON_OBJECT (
+            'amount_rating',
+            rating.amount,
+            'rating_value',
+            rating.rating,
+            'comments',
+            (
+                select
+                    JSON_ARRAYAGG (
+                        JSON_OBJECT (
+                            'id_comment',
+                            comment.id_comment,
+                            'title_text',
+                            comment.title_text,
+                            'rating_value',
+                            comment.rating_value,
+                            'text_comment',
+                            comment.comment_text,
+                            'created_at',
+                            comment.created_at
+                        )
+                    )
+                FROM
+                    comment
+                where
+                    comment.id_product = product.id
+            )
+        ),
+        'description',
+        JSON_OBJECT (
+            'desc_html',
+            description_product.description_html,
+            'updated_at',
+            description_product.updated_at
+        )
+    )
+FROM
+    product
+    LEFT JOIN category on product.category_id = category.id
+    LEFT JOIN brand on product.brand_id = brand.id
+    LEFT JOIN value_product on product.id = value_product.id_product
+    LEFT JOIN rating on product.id = rating.id_product
+    LEFT JOIN description_product on product.id = description_product.id_product
+WHERE
+    product.id = %s
                 """
+
+
+new_get_product_id = """
+SELECT
+    JSON_OBJECT (
+        'id',
+        product.id,
+        'name',
+        product.name,
+        'stock',
+        product.stock,
+        'warranty',
+        product.warranty,
+        'model',
+        product.model,
+        'featured',
+        product.featured,
+        'active',
+        product.active,
+        'category',
+        category.name,
+        'brand',
+        JSON_OBJECT (
+            'name',
+            brand.name,
+            'logo',
+            brand.brand_logo,
+            'logo_black',
+            brand.brand_logo_black
+        ),
+        'images',
+        (
+            Select
+                JSON_ARRAYAGG (
+                    JSON_OBJECT ('path', image.path, 'index', image.index_image)
+                )
+            from
+                image
+            WHERE
+                image.id_product = product.id
+        ),
+        'value',
+        JSON_OBJECT (
+            'price_now',
+            value_product.price_now,
+            'common_price',
+            value_product.common_price,
+            'portions',
+            value_product.portions,
+            'fees_monthly',
+            value_product.fees_monthly,
+            'fees_credit',
+            value_product.fees_credit
+        ),
+        'rating',
+        JSON_OBJECT (
+            'amount_rating',
+            rating.amount,
+            'rating_value',
+            rating.rating,
+            'comments',
+            (
+                select
+                    JSON_ARRAYAGG (
+                        JSON_OBJECT (
+                            'id_comment',
+                            comment.id_comment,
+                            'title_text',
+                            comment.title_text,
+                            'rating_value',
+                            comment.rating_value,
+                            'text_comment',
+                            comment.comment_text,
+                            'created_at',
+                            comment.created_at
+                        )
+                    )
+                FROM
+                    comment
+                where
+                    comment.id_product = product.id
+            )
+        ),
+        'description',
+        JSON_OBJECT (
+            'desc_html',
+            description_product.description_html,
+            'updated_at',
+            description_product.updated_at
+        )
+    )
+FROM
+    product
+    LEFT JOIN category on product.category_id = category.id
+    LEFT JOIN brand on product.brand_id = brand.id
+    LEFT JOIN value_product on product.id = value_product.id_product
+    LEFT JOIN rating on product.id = rating.id_product
+    LEFT JOIN description_product on product.id = description_product.id_product
+WHERE
+    product.id = %s
+"""
 
 search_product_name = """
             SELECT
--- api/routers/product.py
+++ api/routers/product.py
@@ -7,7 +7,8 @@ from database.connection import mysql_connection
 import time
 import json
 from models.models import new_description
-from requests.product import get_all_products, get_product_id, search_product_name, get_limit_products, get_limit_products_specific_brand
+from requests.product import get_all_products, get_product_id, search_product_name, get_limit_products, get_limit_products_specific_brand, new_get_product_id, new_get_products_limit
+from functions.product import organize_images_from_products
 
 router = APIRouter()
 
@@ -39,16 +40,16 @@ async def get_limited_products(limit: int):
     try:
         cursor = mysql_connection.cursor(dictionary=True)
 
-        cursor.execute(get_limit_products, (limit,))
-        data = cursor.fetchall()
-        products = []
+        cursor.execute(new_get_products_limit, (limit,))
+        data = cursor.fetchone()
+        # print(data)
+        data = json.loads(data['products'])
 
         for product in data:
-            dict_product = json.loads(product["product"])
-            dict_product["images"] = sorted(dict_product["images"])
-            products.append(dict_product)
-        print(products)
-        return products
+            images = organize_images_from_products(product=product)
+            product['images'] = images
+
+        return data
     except Exception as e:
         return e
     finally:
@@ -60,11 +61,21 @@ async def search_product(product_id: str):
     try:
         cursor = mysql_connection.cursor(dictionary=True)
 
-        cursor.execute(get_product_id,
-                       (product_id,))
+        print(product_id)
+        # print(get_product_id)
+        # return get_product_id
+
+        cursor.execute(new_get_product_id, (product_id,))
+        print('passei pelo cursor')
         data = cursor.fetchone()
-        return json.loads(data["product"])
+        product = json.loads(data["product"])
+        print(data)
+        images = organize_images_from_products(product=product)
+        product['images'] = images
+        print(product)
+        return product
     except Exception as e:
+        print(e)
         return e
 
 
