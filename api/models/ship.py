99c72103a35669645f45c6a88a47dede7c971f8a
LucasLfs2004 <lucas.lfs2004@gmail.com>
2024-03-07 22:54:41 -0300

    Rota de delete e patch para informações de entrega

--- api/models/ship.py
+++ api/models/ship.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel
 from typing import Optional
 
 
-class ship_info(BaseModel):
+class ship_info_post(BaseModel):
     name: str
     phone_number: str
     district: str
@@ -14,3 +14,18 @@ class ship_info(BaseModel):
     complement: str
     number: int
     principal_ship: Optional[bool] = False
+
+
+class ship_info_patch(BaseModel):
+    name: str
+    phone_number: str
+    district: str
+    city: str
+    state: str
+    receiver: str
+    street: str
+    cep: str
+    complement: str
+    number: int
+    principal_ship: Optional[bool] = False
+    id: Optional[str] = ''
--- api/routers/ship.py
+++ api/routers/ship.py
@@ -2,7 +2,7 @@ import uuid
 from fastapi import HTTPException, APIRouter, Form, Depends
 from database.connection import mysql_connection
 import json
-from models.ship import ship_info
+from models.ship import ship_info_patch, ship_info_post
 from dependencies import token
 router = APIRouter()
 
@@ -21,7 +21,7 @@ async def set_ship_cart(region: str):
 
 
 @router.post('/ship-info', tags=['User'])
-async def set_ship_info(ship: ship_info, current_user: int = Depends(token.get_current_user)):
+async def set_ship_info(ship: ship_info_post, current_user: int = Depends(token.get_current_user)):
     try:
         cursor = mysql_connection.cursor(dictionary=True)
         id = int.from_bytes(
@@ -34,6 +34,40 @@ async def set_ship_info(ship: ship_info, current_user: int = Depends(token.get_c
             cursor.execute(
                 'UPDATE person SET principal_ship_id = %s WHERE id = %s', (id, current_user))
             mysql_connection.commit()
+        cursor.close()
+        return True
+
+    except Exception as e:
+        mysql_connection.rollback()
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@router.patch('/ship-info', tags=['User'])
+async def set_ship_info(ship: ship_info_patch, current_user: int = Depends(token.get_current_user)):
+    try:
+        cursor = mysql_connection.cursor(dictionary=True)
+        cursor.execute('UPDATE ship_info SET name = %s, phone_number = %s, district = %s, cidade = %s, estado = %s, receiver = %s, street = %s, cep = %s, complement = %s, number = %s WHERE id = %s and id_person = %s', (
+            ship.name, ship.phone_number, ship.district, ship.city, ship.state, ship.receiver, ship.street, ship.cep, ship.complement, ship.number, ship.id, current_user))
+        mysql_connection.commit()
+
+        if ship.principal_ship:
+            cursor.execute(
+                'UPDATE person SET principal_ship_id = %s WHERE id = %s', (ship.id, current_user))
+            mysql_connection.commit()
+        cursor.close()
+        return True
+
+    except Exception as e:
+        mysql_connection.rollback()
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@router.delete('/ship-info/{id_ship}', tags=['User'])
+async def set_ship_info(id_ship: str, current_user: int = Depends(token.get_current_user)):
+    try:
+        cursor = mysql_connection.cursor(dictionary=True)
+        cursor.execute('DELETE FROM ship_info WHERE id = %s', (id_ship,))
+        mysql_connection.commit()
         cursor.close()
         return True
 
