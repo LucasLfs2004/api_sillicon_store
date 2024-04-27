80c6a1b162515a88fcff97a1d16f70ac22743516
LucasLfs2004 <lucas.lfs2004@gmail.com>
2024-04-26 22:22:26 -0300

    rota para banners

--- api/routers/banner.py
+++ api/routers/banner.py
@@ -31,7 +31,7 @@ async def get_banners():
 
 @router.post("/banner", tags=['Admin', 'Banner'])
 # , current_user: int = Depends(token.is_admin)):
-async def create_banner(link_redirect: str = Form(), image_web: UploadFile = File(), image_mobile: UploadFile = File()):
+async def create_banner(link_redirect: str = Form(), image_web: UploadFile = File(), image_mobile: UploadFile = File(), current_user: int = Depends(token.is_admin)):
     try:
         cursor = mysql_connection.cursor(dictionary=True)
         id = int.from_bytes(
@@ -71,65 +71,97 @@ async def create_banner(link_redirect: str = Form(), image_web: UploadFile = Fil
         cursor.close()
 
 
-@ router.patch("/brand", tags=["Marca"])
-async def update_brand(id: str = Form(), name: str = Form(None), brand_logo: UploadFile = File(None), brand_logo_black: UploadFile = File(None), current_user: int = Depends(token.is_admin)):
+@ router.patch("/banner", tags=["Admin", "Banner"])
+async def update_brand(id: str = Form(), link_redirect: str = Form(None), image_web: UploadFile = File(None), image_mobile: UploadFile = File(None), active: str = Form(None), current_user: int = Depends(token.is_admin)):
     try:
         cursor = mysql_connection.cursor(dictionary=True)
 
-        cursor.execute("SELECT * FROM BRAND WHERE id = %s", (id,))
-        old_brand = cursor.fetchone()
+        cursor.execute("SELECT * FROM banner WHERE id = %s", (id,))
+        old_banner = cursor.fetchone()
 
-        new_brand = {}
+        new_banner = {}
 
-        if name is not None:
-            new_brand["name"] = name
+        print(old_banner['link'])
+        print(link_redirect)
+        if link_redirect is not None:
+            new_banner["link"] = link_redirect
         else:
-            new_brand["name"] = old_brand['name']
+            new_banner["link"] = old_banner['link']
 
-        if brand_logo is not None:
-            filename_brand_logo = str(time.time()) + \
-                brand_logo.filename.replace(' ', '_')
-            file_path = os.path.join(upload_folder, filename_brand_logo)
+        print('passei pelo primeiro if')
+
+        if active is not None:
+            new_banner["active"] = True
+        else:
+            new_banner["active"] = old_banner['active']
+
+        if image_web is not None:
+            filename_image_web = str(time.time()) + \
+                image_web.filename.replace(' ', '_')
+            file_path = os.path.join(upload_folder, filename_image_web)
             with open(file_path, "wb") as f:
-                f.write(brand_logo.file.read())
-            print(filename_brand_logo)
-            new_brand["brand_logo"] = str(filename_brand_logo)
+                f.write(image_web.file.read())
+            print(filename_image_web)
+            new_banner["img_banner_web"] = str(filename_image_web)
         else:
-            new_brand["brand_logo"] = old_brand["brand_logo"]
+            new_banner["img_banner_web"] = old_banner["img_banner_web"]
 
-        if brand_logo_black is not None:
-            filename_brand_logo_black = str(
-                time.time()) + brand_logo_black.filename.replace(' ', '_')
-            file_path = os.path.join(upload_folder, filename_brand_logo_black)
+        if image_mobile is not None:
+            filename_image_mobile = str(
+                time.time()) + image_mobile.filename.replace(' ', '_')
+            file_path = os.path.join(upload_folder, filename_image_mobile)
             with open(file_path, "wb") as f:
-                f.write(brand_logo_black.file.read())
-            print(filename_brand_logo_black)
-            new_brand["brand_logo_black"] = str(filename_brand_logo_black)
+                f.write(image_mobile.file.read())
+            print(filename_image_mobile)
+            new_banner["img_banner_mobile"] = str(filename_image_mobile)
         else:
-            new_brand["brand_logo_black"] = old_brand["brand_logo_black"]
+            new_banner["img_banner_mobile"] = old_banner["img_banner_mobile"]
 
         cursor.execute(
-            "UPDATE BRAND SET name = %s, brand_logo = %s, brand_logo_black = %s WHERE id = %s",
-            (new_brand["name"], new_brand["brand_logo"],
-             new_brand["brand_logo_black"], id)
+            "UPDATE banner SET link = %s, img_banner_web = %s, img_banner_mobile = %s, active = %s WHERE id = %s",
+            (new_banner["link"], new_banner["img_banner_web"],
+             new_banner["img_banner_mobile"], new_banner["active"], id)
         )
         mysql_connection.commit()
 
         print("Tudo certo até aqui, iniciando a exclusão das imagens antigas")
 
         # Removendo imagens antigas da api
-        if brand_logo is not None:
-            filename_logo = os.path.join(
-                upload_folder + '/' + old_brand["brand_logo"])
-            os.remove(filename_logo)
-        if brand_logo_black is not None:
-            filename_logo_black = os.path.join(
-                update_brand + '/' + old_brand["brand_logo_black"])
-            os.remove(filename_logo_black)
+        if image_web is not None:
+            filename_banner_web = os.path.join(
+                upload_folder + '/' + old_banner["img_banner_web"])
+            os.remove(filename_banner_web)
+        if image_mobile is not None:
+            filename_banner_mobile = os.path.join(
+                update_brand + '/' + old_banner["img_banner_mobile"])
+            os.remove(filename_banner_mobile)
 
-        cursor.execute("SELECT * FROM BRAND WHERE NAME = %s", (name,))
-        brand_updated = cursor.fetchone()
         cursor.close()
+        return True
+
+    except Exception as e:
+        # Em caso de erro, cancelar a transação e retornar uma resposta de erro
+        mysql_connection.rollback()
+        print(e)
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@ router.patch("/banner/active", tags=["Admin", "Banner"])
+async def handle_active_banner(id: str):
+    try:
+        cursor = mysql_connection.cursor(dictionary=True)
+
+        cursor.execute("SELECT active FROM banner WHERE id = %s", (id,))
+        banner_data = cursor.fetchone()
+
+        if banner_data['active'] == 0:
+            cursor.execute(
+                'UPDATE banner SET active = %s WHERE id = %s', (1, id))
+        else:
+            cursor.execute(
+                'UPDATE banner SET active = %s WHERE id = %s', (0, id))
+
+        mysql_connection.commit()
         return True
 
     except Exception as e:
@@ -137,71 +169,23 @@ async def update_brand(id: str = Form(), name: str = Form(None), brand_logo: Upl
         return e
 
 
-@ router.put("/brand", tags=["Marca"])
-async def update_brand(id: str = Form(), name: str = Form(), brand_logo: UploadFile = File(), brand_logo_black: UploadFile = File()):
+@ router.delete("/banner/{id}", tags=["Admin", "Banner"])
+async def delete_banner(id: str, current_user: int = Depends(token.is_admin)):
     try:
         cursor = mysql_connection.cursor(dictionary=True)
-
-        cursor.execute("SELECT * FROM BRAND WHERE id = %s", (id,))
-        old_brand = cursor.fetchone()
-
-        filename_brand_logo = str(time.time()) + \
-            brand_logo.filename.replace(' ', '_')
-        file_path = os.path.join(upload_folder, filename_brand_logo)
-        with open(file_path, "wb") as f:
-            f.write(brand_logo.file.read())
-        print(filename_brand_logo)
-        filename_brand_logo_black = str(
-            time.time()) + brand_logo_black.filename.replace(' ', '_')
-        file_path = os.path.join(upload_folder, filename_brand_logo_black)
-        with open(file_path, "wb") as f:
-            f.write(brand_logo_black.file.read())
-        print(filename_brand_logo_black)
-
-        cursor.execute(
-            "UPDATE BRAND SET name = %s, brand_logo = %s, brand_logo_black = %s WHERE id = %s",
-            (name, filename_brand_logo, filename_brand_logo_black, id)
-        )
-        mysql_connection.commit()
-
-        print("Tudo certo até aqui, iniciando a exclusão das imagens antigas")
-
-        # Removendo imagens antigas da api
-        filename_logo = os.path.join(
-            upload_folder + "/" + old_brand["brand_logo"])
-        os.remove(filename_logo)
-        filename_logo_black = os.path.join(
-            upload_folder + "/" + old_brand["brand_logo_black"])
-        os.remove(filename_logo_black)
-        mysql_connection.commit()
-
-        cursor.execute("SELECT * FROM BRAND WHERE NAME = %s", (name,))
-        brand_updated = cursor.fetchone()
-        cursor.close()
-        return brand_updated
-
-    except Exception as e:
-        print(e)
-        return e
-
-
-@ router.delete("/brand/{id}", tags=["Marca"])
-async def delete_brand(id: str, current_user: int = Depends(token.is_admin)):
-    try:
-        cursor = mysql_connection.cursor(dictionary=True)
-        cursor.execute("SELECT * FROM BRAND WHERE id = %s", (id,))
+        cursor.execute("SELECT * FROM banner WHERE id = %s", (id,))
         data = cursor.fetchone()
 
-        cursor.execute("DELETE FROM BRAND WHERE id LIKE %s", (id,))
+        cursor.execute("DELETE FROM banner WHERE id = %s", (id,))
 
-        if data['brand_logo'] is not None:
+        if data['img_banner_web'] is not None:
             filename_logo = os.path.join(
-                upload_folder + "/" + data["brand_logo"])
+                upload_folder + "/" + data["img_banner_web"])
             os.remove(filename_logo)
 
-        if data['brand_logo_black'] is not None:
+        if data['img_banner_mobile'] is not None:
             filename_logo_black = os.path.join(
-                upload_folder + "/" + data["brand_logo_black"])
+                upload_folder + "/" + data["img_banner_mobile"])
             os.remove(filename_logo_black)
         mysql_connection.commit()
 
