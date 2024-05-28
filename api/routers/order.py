abf4006c8838660c58158c40ff3f743c31de854f
LucasLfs2004 <lucas.lfs2004@gmail.com>
2024-05-28 20:42:36 -0300

    criação para chamada de pedido por id

--- api/routers/order.py
+++ api/routers/order.py
@@ -8,7 +8,7 @@ import time
 import json
 from dependencies import token
 from models.product import update_product_model, new_description
-from requests.order import select_purchase_order
+from requests.order import select_purchase_order, select_purchase_order_id
 from functions.product import organize_images_from_products
 
 router = APIRouter()
@@ -27,6 +27,27 @@ async def get_limited_products(current_user: int = Depends(token.get_current_use
             for product in item['items']:
                 images = organize_images_from_products(product=product)
                 product['images'] = images
+
+        return data
+    except Exception as e:
+        mysql_connection.rollback()
+        raise HTTPException(status_code=500, detail=str(e))
+    finally:
+        cursor.close()
+
+
+@router.get("/purchase-orders/{id}", tags=['Pedidos'])
+async def get_limited_products(id: str, current_user: int = Depends(token.get_current_user)):
+    try:
+        cursor = mysql_connection.cursor(dictionary=True)
+
+        cursor.execute(select_purchase_order_id, (id,))
+        data = cursor.fetchone()
+        data = json.loads(data['purchases'])
+
+        for product in data['items']:
+            images = organize_images_from_products(product=product)
+            product['images'] = images
 
         return data
     except Exception as e:
